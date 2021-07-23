f96113dd7c0e07989b7dad7505156858bf403833
JunxianLong <30436924+JunxianLong@users.noreply.github.com>
2021-07-23 15:06:42 +0800

    Create search.go

---
+++ week02/search.go
@@ -0,0 +1,16 @@
+// GetUserNameByID 根据ID获取用户名
+func (*seqDao) GetUserNameByID(userID int)(string ,error) {
+	tx := DB.Begin()
+	defer tx.Rollback()
+
+	var userName string
+	err := DB.Raw("SELECT user_name FROM user_info WHERE user_id = ?", userID).
+		Row().Scan(&userName)
+	if !errors.Is(err,sql.ErrNoRows){
+		return "",pkgErrors.Wrap(err,"query error")
+	}
+	/*
+		do something
+	 */
+	return "",nil
+}
